17e6e9fd6090f6c150fc4a86cd3a9e3fb4e89a76
Mehrdad Karami <mehrdad.karami@ippen-digital.de>
2022-04-08 17:01:44 +0200

    renamed application name

--- src/main/java/com/metao/springboot/redis/SpringBootRedisApplication.java
+++ src/main/java/com/metao/springboot/redis/SpringBootRedisApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(proxyBeanMethods = false)
-public class SpringBootJpaApplication {
+public class SpringBootRedisApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootJpaApplication.class, args);
+        SpringApplication.run(SpringBootRedisApplication.class, args);
     }
 
 }
